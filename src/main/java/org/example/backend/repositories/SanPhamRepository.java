1c227eb12ec2d7f2199293e90639190223592dc0
VuAnhTai1111 <vuanhtai06011979@gmail.com>
2024-09-24 22:42:47 +0700

    haiz

--- src/main/java/org/example/backend/repositories/SanPhamRepository.java
+++ src/main/java/org/example/backend/repositories/SanPhamRepository.java
@@ -1,9 +1,17 @@
 package org.example.backend.repositories;
 
+import org.example.backend.dto.response.SanPham.SanPhamResponse;
 import org.example.backend.models.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SanPhamRepository extends JpaRepository<SanPham, UUID> {
+    @Query("""
+    select new org.example.backend.dto.response.SanPham (s.ma,s.ten,s.ngayTao,spct.soLuong,s.trangThai)
+    from SanPham s join SanPhamChiTiet spct on spct.idSanPham=s.id
+""")
+    List<SanPhamResponse> getAllSanPham();
 }
