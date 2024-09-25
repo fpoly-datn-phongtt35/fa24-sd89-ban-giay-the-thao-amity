31aa8778dbf9efdda7a0cc03cb848da915f40eb5
VuAnhTai1111 <vuanhtai06011979@gmail.com>
2024-09-25 14:13:21 +0700

    sua sp

--- src/main/java/org/example/backend/repositories/SanPhamRepository.java
+++ src/main/java/org/example/backend/repositories/SanPhamRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 import java.util.UUID;
 
 public interface SanPhamRepository extends JpaRepository<SanPham, UUID> {
-    @Query("""
-    select new org.example.backend.dto.response.SanPham (s.ma,s.ten,s.ngayTao,spct.soLuong,s.trangThai)
-    from SanPham s join SanPhamChiTiet spct on spct.idSanPham=s.id
-""")
-    List<SanPhamResponse> getAllSanPham();
+//    @Query("""
+//    select new org.example.backend.dto.response.SanPham (s.ma,s.ten,s.ngayTao,spct.soLuong,s.trangThai)
+//    from SanPham s join SanPhamChiTiet spct on spct.idSanPham=s.id
+//""")
+//    List<SanPhamResponse> getAllSanPham();
 }
