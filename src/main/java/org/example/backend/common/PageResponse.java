fc0c4d6b1ebb508460c5f7bbdaff833dd193a621
Ina <anhlnph42539@fpt.edu.vn>
2024-10-07 00:57:17 +0700

    update lai code

---
+++ src/main/java/org/example/backend/common/PageResponse.java
@@ -0,0 +1,22 @@
+package org.example.backend.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PageResponse<T> implements Serializable {
+
+    private int page;
+
+    private int size;
+
+    private long totalPage;
+
+    private transient T items;
+
+}
--- src/main/java/org/example/backend/repositories/NguoiDungRepository.java
+++ src/main/java/org/example/backend/repositories/NguoiDungRepository.java
@@ -1,6 +1,7 @@
 package org.example.backend.repositories;
 
 import jakarta.transaction.Transactional;
+import org.example.backend.common.PageResponse;
 import org.example.backend.dto.response.NhanVien.NhanVienRespon;
 import org.example.backend.dto.response.khachHang.KhachHangResponse;
 import org.example.backend.models.NguoiDung;
@@ -9,16 +10,23 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface NguoiDungRepository extends JpaRepository<NguoiDung, UUID> {
     @Query("""
-    select new org.example.backend.dto.response.NhanVien.NhanVienRespon(nd.id,nd.ma,nd.email,nd.sdt,nd.matKhau,nd.ten,nd.diaChi,nd.ngaySinh,nd.gioiTinh,nd.hinhAnh,nd.cccd,nd.chucVu,nd.trangThai,nd.deleted)
-    from NguoiDung nd where nd.chucVu = 'nhanvien' and nd.deleted = false   
+    select new org.example.backend.dto.response.NhanVien.NhanVienRespon(
+        nd.id, nd.ma, nd.email, nd.sdt, nd.matKhau, nd.ten, nd.diaChi, nd.ngaySinh, 
+        nd.gioiTinh, nd.hinhAnh, nd.cccd, nd.chucVu, nd.trangThai, nd.deleted
+    )
+    from NguoiDung nd
+    where nd.chucVu = 'nhanvien' and nd.deleted = false
+    order by nd.ngayTao DESC
 """)
-    List<NhanVienRespon> getAllNhanVien();
+    Page<NhanVienRespon> getAllNhanVien(Pageable pageable);
+
 
     @Transactional
     @Modifying
@@ -33,9 +41,68 @@ public interface NguoiDungRepository extends JpaRepository<NguoiDung, UUID> {
     @Query("""
         select new org.example.backend.dto.response.NhanVien.NhanVienRespon(nd.id, nd.ma, nd.email, nd.sdt, nd.matKhau, nd.ten, nd.diaChi, nd.ngaySinh, nd.gioiTinh, nd.hinhAnh, nd.cccd, nd.chucVu, nd.trangThai, nd.deleted)
         from NguoiDung nd 
-        where nd.chucVu = 'nhanvien' and nd.deleted = true
+        where nd.chucVu = 'nhanvien' and nd.deleted = false 
+        order by nd.ngayTao DESC 
     """)
     Page<NhanVienRespon> getAllNhanVienPage(Pageable pageable);
+
+    @Query("""
+    select new org.example.backend.dto.response.NhanVien.NhanVienRespon(nd.id, nd.ma, nd.email, nd.sdt, nd.matKhau, nd.ten, nd.diaChi, nd.ngaySinh, nd.gioiTinh, nd.hinhAnh, nd.cccd, nd.chucVu, nd.trangThai, nd.deleted)
+    from NguoiDung nd
+    where (nd.ma like :name or nd.ten like :name or nd.sdt like :name) 
+      and nd.chucVu = 'nhanvien' 
+      and nd.deleted = false
+""")
+    List<NhanVienRespon> searchUserNhanVien(String name);
+
+    @Query("""
+    select new org.example.backend.dto.response.NhanVien.NhanVienRespon(nd.id, nd.ma, nd.email, nd.sdt, nd.matKhau, nd.ten, nd.diaChi, nd.ngaySinh, nd.gioiTinh, nd.hinhAnh, nd.cccd, nd.chucVu, nd.trangThai, nd.deleted)
+    from NguoiDung nd
+    where  nd.chucVu = 'nhanvien'  and nd.deleted = false
+    order by nd.ten DESC 
+""")
+    List<NhanVienRespon>  sortNhanVien();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //    KhachHang
     @Query("""
