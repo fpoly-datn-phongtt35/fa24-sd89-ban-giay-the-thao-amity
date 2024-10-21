e97fb45e74eed4a8c89f73f6ffd9969f390af91d
Ina <anhlnph42539@fpt.edu.vn>
2024-10-21 15:23:31 +0700

    update code

--- src/main/java/org/example/backend/repositories/HoaDonChiTietRepository.java
+++ src/main/java/org/example/backend/repositories/HoaDonChiTietRepository.java
@@ -24,21 +24,5 @@ public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet, UU
 """)
     Page<QuanLyDonHangRespose> getByPageHoaDon(Pageable pageable);
 
-//    @Query("""
-//        select new org.example.backend.dto.response.quanLyDonHang.QuanLyDonHangRespose(
-//            qldh.id,qldh.idHoaDon.ma,qldh.soLuong,qldh.idHoaDon.tongTien,qldh.idHoaDon.idNguoiDung.ten,qldh.ngayTao,qldh.idHoaDon.loaiHoaDon,qldh.trangThai,qldh.deleted
-//        )
-//        from HoaDonChiTiet qldh
-//        where qldh.deleted = false
-//         and (
-//            (:keyword is null or :keyword = '' or
-//            lower(qldh.idHoaDon.ma) like lower(concat('%', :keyword, '%'))
-//          )
-//        )
-//
-//        and (:gioiTinh is null or :gioiTinh = '' or nd.gioiTinh = :gioiTinh)
-//        and (:trangThai is null or :trangThai = '' or nd.trangThai = :trangThai)
-//        order by qldh.ngayTao desc
-//""")
-//    List<QuanLyDonHangRespose> searchHoaDon();
+
 }
