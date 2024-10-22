bf316ff2763a09cd661a830f2e4305f60e3549c8
Ina <anhlnph42539@fpt.edu.vn>
2024-10-22 13:02:51 +0700

    fix bug

--- src/main/java/org/example/backend/services/NguoiDungService.java
+++ src/main/java/org/example/backend/services/NguoiDungService.java
@@ -61,7 +61,7 @@ public class NguoiDungService extends GenericServiceImpl<NguoiDung , UUID> {
         return nguoiDungRepository.searchUserNhanVien(keyword, gioiTinh, trangThai);
 
 
-        return nhanVienRespository.searchUserNhanVien(keyword, gioiTinh, trangThai);
+
 
     }
 
