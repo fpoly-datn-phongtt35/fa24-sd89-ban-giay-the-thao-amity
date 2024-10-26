b11875e8a39cde2cb6d5f1323ce2adeb5be58996
VuAnhTai1111 <vuanhtai06011979@gmail.com>
2024-10-26 16:35:47 +0700

    sp vs dggct

--- src/main/java/org/example/backend/dto/request/sanPham/HinhAnhRequest.java
+++ src/main/java/org/example/backend/dto/request/sanPham/HinhAnhRequest.java
@@ -1,7 +1,17 @@
 package org.example.backend.dto.request.sanPham;
 
-import java.time.Instant;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backend.models.SanPhamChiTiet;
 
+import java.time.Instant;
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HinhAnhRequest {
     private String ma;
 
@@ -22,81 +32,7 @@ public class HinhAnhRequest {
 
     private String trangThai;
 
-    public HinhAnhRequest() {
-    }
+    private SanPhamChiTiet idSanPhamChiTiet;
 
-    public HinhAnhRequest(String ma, String ten, String url, Instant ngayTao, Instant ngaySua, String nguoiTao, String nguoiSua, String trangThai) {
-        this.ma = ma;
-        this.ten = ten;
-        this.url = url;
-        this.ngayTao = ngayTao;
-        this.ngaySua = ngaySua;
-        this.nguoiTao = nguoiTao;
-        this.nguoiSua = nguoiSua;
-        this.trangThai = trangThai;
-    }
 
-    public String getMa() {
-        return ma;
-    }
-
-    public void setMa(String ma) {
-        this.ma = ma;
-    }
-
-    public String getTen() {
-        return ten;
-    }
-
-    public void setTen(String ten) {
-        this.ten = ten;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Instant getNgayTao() {
-        return ngayTao;
-    }
-
-    public void setNgayTao(Instant ngayTao) {
-        this.ngayTao = ngayTao;
-    }
-
-    public Instant getNgaySua() {
-        return ngaySua;
-    }
-
-    public void setNgaySua(Instant ngaySua) {
-        this.ngaySua = ngaySua;
-    }
-
-    public String getNguoiTao() {
-        return nguoiTao;
-    }
-
-    public void setNguoiTao(String nguoiTao) {
-        this.nguoiTao = nguoiTao;
-    }
-
-    public String getNguoiSua() {
-        return nguoiSua;
-    }
-
-    public void setNguoiSua(String nguoiSua) {
-        this.nguoiSua = nguoiSua;
-    }
-
-    public String getTrangThai() {
-        return trangThai;
-    }
-
-    public void setTrangThai(String trangThai) {
-        this.trangThai = trangThai;
-    }
 }
