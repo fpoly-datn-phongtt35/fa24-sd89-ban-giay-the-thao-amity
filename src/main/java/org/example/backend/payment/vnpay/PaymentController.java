4d86b2f4f5b8d1c1324c13b9ce9b5be1d180eaa5
VuAnhTai1111 <vuanhtai06011979@gmail.com>
2024-11-25 12:51:13 +0700

    ud

--- src/main/java/org/example/backend/payment/vnpay/PaymentController.java
+++ src/main/java/org/example/backend/payment/vnpay/PaymentController.java
@@ -1,12 +1,15 @@
 package org.example.backend.payment.vnpay;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.common.ResponseObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("${spring.application.api-prefix}/payment")
@@ -17,13 +20,27 @@ public class PaymentController {
     public ResponseObject<PaymentDTO.VNPayResponse> pay(HttpServletRequest request) {
         return new ResponseObject<>(HttpStatus.OK, "Success", paymentService.createVnPayPayment(request));
     }
+//    @GetMapping("/vn-pay-callback")
+//    public ResponseObject<PaymentDTO.VNPayResponse> payCallbackHandler(HttpServletRequest request) {
+//        String status = request.getParameter("vnp_ResponseCode");
+//        if (status.equals("00")) {
+//            return new ResponseObject<>(HttpStatus.OK, "Success", new PaymentDTO.VNPayResponse("00", "Success", ""));
+//        } else {
+//            return new ResponseObject<>(HttpStatus.BAD_REQUEST, "Failed", null);
+//        }
+//    }
+
     @GetMapping("/vn-pay-callback")
-    public ResponseObject<PaymentDTO.VNPayResponse> payCallbackHandler(HttpServletRequest request) {
+    public void payCallbackHandler(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String status = request.getParameter("vnp_ResponseCode");
-        if (status.equals("00")) {
-            return new ResponseObject<>(HttpStatus.OK, "Success", new PaymentDTO.VNPayResponse("00", "Success", ""));
+        String amount = request.getParameter("vnp_Amount");
+        String transactionNo = request.getParameter("vnp_TransactionNo");
+
+        // Kiểm tra trạng thái giao dịch
+        if ("00".equals(status)) {
+            response.sendRedirect("http://localhost:3000/payment/result?status=success&amount=" + amount + "&transactionNo=" + transactionNo);
         } else {
-            return new ResponseObject<>(HttpStatus.BAD_REQUEST, "Failed", null);
+            response.sendRedirect("http://localhost:3000/payment/result?status=failed");
         }
     }
 }
